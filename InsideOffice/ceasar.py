5c531dd033458d0a3056ef3562bd09df65c4a07d
jesyfox <jesyfoxbs@gmail.com>
2018-12-15 19:29:53 +0200

    added raw ceasar algorythm

--- InsideOffice/ceasar.py
+++ InsideOffice/ceasar.py
@@ -1,12 +1,18 @@
 
-def crypto_ceasar(text, key, step=3):
-    pass
+def crypto_ceasar(text: str, key: str, step: int = 3):
+    hidden_text = []
+    for symbol in text:
+        symbol_index = key.find(symbol)
+        crypto_index = symbol_index + step
+        hidden_text.append(key[crypto_index])
 
+    return ''.join(hidden_text)
 
 def main():
     key_word = 'abcdefghijklmnopqrstuvwxyz'
-    text_to_hide = input('Type a text: ')
-    crypto_ceasar(text_to_hide, key_word)
+    text_to_hide = input('Type a text: ').lower()
+    print(crypto_ceasar(text_to_hide, key_word))
+
 
 if __name__ == '__main__':
     main()

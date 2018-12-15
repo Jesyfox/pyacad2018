6b626b17953cad93769cd16039d4448047444071
jesyfox <jesyfoxbs@gmail.com>
2018-12-15 13:33:17 +0200

    feature: started work at ceasar crypt

---
+++ InsideOffice/ceasar.py
@@ -0,0 +1,12 @@
+
+def crypto_ceasar(text, key, step=3):
+    pass
+
+
+def main():
+    key_word = 'abcdefghijklmnopqrstuvwxyz'
+    text_to_hide = input('Type a text: ')
+    crypto_ceasar(text_to_hide, key_word)
+
+if __name__ == '__main__':
+    main()

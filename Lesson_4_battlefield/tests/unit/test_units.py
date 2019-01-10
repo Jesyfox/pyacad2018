2d2910b884847a0332b89bbdd0d5e371f9de81a6
jesyfox <jesyfoxbs@gmail.com>
2019-01-10 21:43:16 +0200

    feature: added tests to units

--- Lesson_4_battlefield/tests/unit/test_units.py
+++ Lesson_4_battlefield/tests/unit/test_units.py
@@ -1,6 +1,11 @@
 import unittest
 from unittest.mock import patch
-from unit.units import Unit
+from battlefield.unit.units import Unit
+
+available_units = [
+    ('soldier', {}),
+    ('vehicle', {'operator': 'soldier', 'u_count': 3})
+]
 
 
 class TestStringMethods(unittest.TestCase):
@@ -11,5 +16,41 @@ class TestStringMethods(unittest.TestCase):
 
     def test_is_dead(self):
         unit = Unit.new('soldier')
-        unit.take_damage()
+        unit.take_damage(unit.health)
         self.assertFalse(unit.is_alive)
+
+    def test_damage_zero_exp(self):
+        unit = Unit.new('soldier')
+        self.assertLess(0, unit.damage)
+
+    def test_damage_max_exp(self):
+        unit = Unit.new('soldier')
+        for i in range(50):
+            unit.exp_increase()
+        self.assertLess(0, unit.damage)
+
+    def test_damage_low_health(self):
+        unit = Unit.new('soldier')
+        unit.take_damage((unit.health-1))
+        self.assertLess(0, unit.damage)
+
+    def test_attack_success_zero_exp(self):
+        unit = Unit.new('soldier')
+        self.assertLess(0, unit.attack_success)
+
+    def test_attack_success_max_exp(self):
+        unit = Unit.new('soldier')
+        for i in range(50):
+            unit.exp_increase()
+        self.assertLess(0, unit.attack_success)
+
+    def test_attack_success_low_health(self):
+        unit = Unit.new('soldier')
+        unit.take_damage(unit.health - 1)
+        self.assertLess(0, unit.attack_success)
+
+    def test_max_experience(self):
+        unit = Unit.new('soldier')
+        for i in range(1000):
+            unit.exp_increase()
+        self.assertLessEqual(unit.experience, 50)
